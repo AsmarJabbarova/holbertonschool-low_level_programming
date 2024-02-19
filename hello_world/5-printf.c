4a9df8d09a86aec3218c11f5d87122fb704923bc
AsmarJabbarova <bd6p3ncs2@code.edu.az>
2024-02-19 05:26:06 -0800

    added

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
@@ -6,8 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-	printf("%s","with proper grammar, but the outcome is a piece of art,/n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
+
