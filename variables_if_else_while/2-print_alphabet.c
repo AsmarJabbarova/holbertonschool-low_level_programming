2314e43e59ff652737cbab585723ac22454cefe7
AsmarJabbarova <bd6p3ncs@code.edu.az>
2024-02-22 12:43:06 +0000

    added

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Enrty point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	char a = 'a';
+	while(a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
