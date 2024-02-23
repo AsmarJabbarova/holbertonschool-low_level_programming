1b23a20d9bc7a8729f623aa92be8cd9b8e90ccdc
AsmarJabbarova <bd6p3ncs2@code.edu.az>
2024-02-23 11:50:32 +0000

    added

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#inlude <stdio.h>
+/**
+ * main -Prints the alphabet in lowercase, then in uppercase.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	cahr c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		
+		}
+		c++;
+	
+	}
+	putchar('\n');
+	return (0);
+
+}
