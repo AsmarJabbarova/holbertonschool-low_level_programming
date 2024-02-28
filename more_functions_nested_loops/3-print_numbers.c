e3ab6108d50adba4fe0ed7afe04043de655e654c
AsmarJabbarova <bd6p3ncs2@code.edu.az>
2024-02-28 14:39:48 +0000

    added

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - that print numbers
+ */
+void print_numbers(void)
+{
+	int number = 48;
+	for (number = 48; number < 58; number++)
+	{
+		_putchar(number);
+	}
+	_putchar(10);
+}
