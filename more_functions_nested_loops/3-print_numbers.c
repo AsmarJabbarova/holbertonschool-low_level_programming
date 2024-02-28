a833494e335b1744c1c8025717bdeaaf2d7e6dd8
AsmarJabbarova <bd6p3ncs2@code.edu.az>
2024-02-28 15:05:11 +0000

    added

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * print_numbers -that print numbers
+ * print_numbers - that print numbers
  */
 void print_numbers(void)
 {
 	int i = 0;
+
 	for (; i < 10; i++)
 	{
-		_putcahr(48 +i);
+		_putchar(48 + i);
 	}
 	_putchar('\n');
 }
