d1a06c6584d75b56bafd04dded16a21912d48b6c
AsmarJabbarova <bd6p3ncs22code.edu.az>
2024-03-03 16:49:58 +0000

    added

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,27 @@
 #include "main.h"
-#include <string.h>
 /**
- * _strncat - connect n elements
- * @dest: destination
- * @src: source
- * @n: count of elements
- * Return: connected string
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, size = strlen(dest);
+	int count = 0, count2 = 0;
 
-	while (*(src + i) != '\0')
+	while (*(dest + count) != '\0')
 	{
-		if (i == n)
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
 			break;
-		*(dest + size + i) = *(src + i);
-		i++;
+		count++;
+		count2++;
 	}
 	return (dest);
 }
