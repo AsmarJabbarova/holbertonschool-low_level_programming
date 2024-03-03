9762737778c896741c81e5e99d620fca288170ad
AsmarJabbarova <bd6p3ncs22code.edu.az>
2024-03-03 17:37:04 +0000

    added

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -7,11 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, n = strlen(dest);
+	int count = 0, count2 = 0;
 
-	do {
-		*(dest + n + i) = *(src + i);
-		i++;
-	} while (*(src + i) != '\0');
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
