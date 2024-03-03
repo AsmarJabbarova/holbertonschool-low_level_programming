cc808407c686c52636f916e65b6d0f4763eaab2c
AsmarJabbarova <bd6p3ncs2@code.edu.az>
2024-03-03 07:27:43 +0000

    added

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -31,3 +33,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+#endif
