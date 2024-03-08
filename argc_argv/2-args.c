98d7cafd7794243a1ddb642552ea9bf35ca3c41c
AsmarJabbarova <bd6p3ncs2@code.edu.az>
2024-03-08 12:40:39 +0000

    added

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - Prints all arguments it receives.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	int arg;
 
 	for (arg = 0; arg < argc; arg++)
-		printf("%\n", argv[arg]);
+		printf("%s\n", argv[arg]);
 
 	return (0);
 }
